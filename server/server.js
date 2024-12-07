6e2b35b8c9b5f8bed9e9e699e4d52dbc6daaedfd
orcas-6 <irouah2@gmail.com>
2024-12-07 19:02:11 +0100

    solved session problems on vercel

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 const express = require("express");
-const cookieSession = require("cookie-session");
 const cors = require("cors");
+const cookieSession = require("cookie-session");
 require("dotenv").config();
 const app = express();
 const routers = require("./routes/routes");
 
 const isProduction = process.env.NODE_ENV === "production";
 
+const allowedOrigins = isProduction
+    ? ["https://blogs-app-bay.vercel.app"]
+    : ["http://127.0.0.1:5173", "http://localhost:5173"];
+
+if (isProduction) {
+    app.set("trust proxy", 1);
+}
+
 app.use(
     cors({
-        origin: [
-            "https://blogs-app-bay.vercel.app",
-            "http://127.0.0.1:5173",
-            "http://localhost:5173",
-        ],
+        origin: allowedOrigins,
         optionsSuccessStatus: 200,
-        methods: ["GET", "POST", "DELETE", "PUT"],
         credentials: true,
     })
 );
@@ -25,8 +28,8 @@ app.use(
         name: "session",
         keys: [process.env.SESSION_SECRET],
         maxAge: 60000 * 60, // 1 hour
-        // sameSite: isProduction ? "None" : "Lax", // Set sameSite to None only in production
-        secure: isProduction, // Set secure to true only in production
+        sameSite: isProduction ? "None" : "Lax",
+        secure: isProduction,
         httpOnly: true,
     })
 );
